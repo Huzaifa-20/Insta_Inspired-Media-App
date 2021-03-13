a11f995308d294cec57064503bf0ccd7179936d3
Huzaifa-20 <huzaifa15192@gmail.com>
2021-03-14 02:23:56 +0500

    Firebase Connected

---
+++ app/src/main/java/com/huzaifa/wallahabibi/EnterPhoneNumberActivity.java
@@ -0,0 +1,14 @@
+package com.huzaifa.wallahabibi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterPhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_phone_number);
+    }
+}
