6d8c33e7bcf21e89d57220b87fefd5be2e35357c
Huzaifa-20 <huzaifa15192@gmail.com>
2021-03-16 21:11:48 +0500

    Lets do this(1)

--- app/src/main/java/com/huzaifa/wallahabibi/StoriesFragment.java
+++ app/src/main/java/com/huzaifa/wallahabibi/StoriesFragment.java
@@ -1,9 +1,12 @@
 package com.huzaifa.wallahabibi;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +14,23 @@ import androidx.fragment.app.Fragment;
 
 public class StoriesFragment extends Fragment {
 
+
+    Context c;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_stories,container,false);
+        c=container.getContext();
+
+        connectViews(v);
+        setListeners();
         return v;
+    }
+
+    private void setListeners() {
+    }
+
+    private void connectViews(View v) {
+
     }
 }
